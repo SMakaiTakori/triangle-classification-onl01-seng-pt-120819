bdc458da5dacfe0a30ddcc11652805f4f01e3378
S. Makai Takori <makait87@gmail.com>
2020-02-01 22:58:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,12 @@ class Triangle
  end
  
  def kind
-  if @side_1+@side_2 <=@side_3 || @side_2+@side_3 <=@side_1 || @side_3+@side_1 <=@side_2
+  if (@side_1+@side_2 <=@side_3) || (@side_2+@side_3 <=@side_1) || (@side_3+@side_1 <=@side_2)
     raise TriangleError
-  elsif @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+  elsif (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
     raise TriangleError
   else
-    if  
+    if  (@side_1 == @side_2) || (@side_2 == @side_3)
     
     
     end
