4b3513a315704951d520a8258df445d8befdf53a
S. Makai Takori <makait87@gmail.com>
2020-02-01 22:50:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,8 @@ class Triangle
  end
  
  def kind
-  if @side_1+@side_2
- 
+  if @side_1+@side_2 <=@side_3 || @side_2+@side_3 <=@side_1 || @side_3+@side_1 <=@side_2
+    raise TriangleError
  end
  
  class TriangleError < StandardError
